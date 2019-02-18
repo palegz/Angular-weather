72aaeb114be2c9fbf65c8a9dc0ade19113a03427
palegz <pauli.manty@gmail.com>
2019-02-18 20:41:03 +0200

    added wind and a description of the current weather

--- src/app/weather.model.ts
+++ src/app/weather.model.ts
@@ -4,11 +4,17 @@ export class Weather {
     public weather: any;
     public temp: any;
     public name: any;
+    public wind: any;
+    public speed: any;
 
     constructor(weatherResponse: any){
         this.main = weatherResponse.main;
         this.temp = weatherResponse.temp;
         this.weather = weatherResponse.weather;
         this.name  = weatherResponse.name;
+        this.wind = weatherResponse.wind;
+        this.speed = weatherResponse.speed;
     }
+
+    
 }
